f6da6fc6501854de0c512361c08a002c408b65b7
E2029-Aydin <aydinsanoz2@gmail.com>
2020-10-28 23:54:52 +0300

    added src(reverse)

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 
-import Main from './src/Main'
+// import Reverse from './src(reverse)/Reverse';
 // import Router from './src(cityYelp)/Router';
+import Main from './src/Main'
 
 import {name as appName} from './app.json';
 
