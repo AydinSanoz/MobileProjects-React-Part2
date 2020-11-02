403b4b4ffc57728012cbc1c5bbd9baa1e4a4fe1e
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-02 18:24:14 +0300

    fibase bug corrected manuelly

--- src(Makeup)/Pages/LoginPage.js
+++ src(Makeup)/Pages/LoginPage.js
@@ -1,32 +1,60 @@
 import React from 'react';
 import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
+import auth from '@react-native-firebase/auth';
 import {MyInput, MyButton} from '../components';
 
-const LoginPage = () => {
+const LoginPage = (props) => {
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const logIn = () => {
+    auth()
+      .signInWithEmailAndPassword(
+        email,
+        password,
+      )
+      .then((response) => {
+        props.navigation.navigate('BottomTab')
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.code)
+        alert(error.code)
+      });
+  };
+
   return (
-    <SafeAreaView style={{flex: 1, justifyContent : 'flex-start'}}>
+    <SafeAreaView style={{flex: 1, justifyContent: 'flex-start'}}>
       <View style={{flex: 1}}>
-        <Text style={{fontSize: 30, textAlign : 'center'}}>Login</Text>
+        <Text style={{fontSize: 30, textAlign: 'center'}}>Login</Text>
 
-        
-          <MyInput placeholder = "Enter Email..." bottom = '#ece1ff' />
-       
+        <MyInput
+          placeholder="Enter Email..."
+          bottom="#ece1ff"
+          onText={(text) => setEmail(text)}
+        />
 
-        <View style = {{flexDirection :'row', borderBottomColor : '#ece1ff', borderBottomWidth : 2,}}>
-          <MyInput  bottom = 'transparent' flex = {1} placeholder = "Password..." password />
-          <MyButton underline = 'none' title = "Forgot ?" />
-
+        <View style={styles.passwordContainer}>
+          <MyInput
+            bottom="transparent"
+            flex={1}
+            placeholder="Password..."
+            password
+            onText={(pass) => setPassword(pass)}
+          />
+          <MyButton underline="none" title="Forgot ?" />
         </View>
-       
-        <View style = {styles.buttonContainer}>
 
-          <MyButton color = 'white' textAlign = 'center' title="Log in" fontSize={20} />
+        <View style={styles.buttonContainer}>
+          <MyButton
+            color="white"
+            textAlign="center"
+            title="Log in"
+            fontSize={20}
+            onPress={logIn}
+          />
         </View>
-       
       </View>
-      {/* <View style = {{flex:1}}>
-
-      </View> */}
     </SafeAreaView>
   );
 };
@@ -34,11 +62,14 @@ const LoginPage = () => {
 export {LoginPage};
 
 const styles = StyleSheet.create({
-    buttonContainer:{
-        backgroundColor : '#000a12',
-        margin : 10,
-        padding: 1,
-        
-        
-    }
-})
+  passwordContainer: {
+    flexDirection: 'row',
+    borderBottomColor: '#ece1ff',
+    borderBottomWidth: 2,
+  },
+  buttonContainer: {
+    backgroundColor: '#000a12',
+    margin: 10,
+    padding: 1,
+  },
+});
