81d43c5506afe808a1f049d049a2468e65a42125
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-02 18:25:54 +0300

    tab navigation and 3 additional pages added

--- src(Makeup)/Makeup.js
+++ src(Makeup)/Makeup.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native' ;
-import { NavigationContainer, StackRouter } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {SafeAreaView, Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import {Home, Login, LoginPage, Brand, Category, Favorites} from './Pages';
 
-import {Home, Login, LoginPage} from './Pages';
+const Tab = createBottomTabNavigator();
 
+function BottomTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Brand" component={Brand} />
+      <Tab.Screen name="Category" component={Category} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+    </Tab.Navigator>
+  );
+}
 
-const Stack = createStackNavigator();  
-
+const Stack = createStackNavigator();
 
 const Makeup = () => {
-    return(
-        <NavigationContainer>
-            <Stack.Navigator screenOptions= {{headerShown : false}}>
-                <Stack.Screen name = "Home" component= {Home}/>
-                <Stack.Screen name = "Login" component= {Login}/>
-                <Stack.Screen name = "LoginPage" component= {LoginPage}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="LoginPage" component={LoginPage} />
+        <Stack.Screen name="BottomTab" component={BottomTab} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Makeup;
---
+++ src(Makeup)/Pages/Brand.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const Brand = () => {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>Brand List</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export {Brand}
