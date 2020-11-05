15cdd1d3d5870ff26ca2726ff8f2fdb51415a8ea
E2029-Aydin <aydinsanoz2@gmail.com>
2020-11-05 18:21:31 +0300

    src(Job) added to projects with api

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ import {AppRegistry} from 'react-native';
 // import Router from './src(cityYelp)/Router';
 // import Router from './src(famuosOnes)/Router'
 // import Main from './src/Main'
-import  Makeup from './src(Makeup)/Makeup'
+// import  Makeup from './src(Makeup)/Makeup'
+// import Router from './src(JobApi)/Router'
+import  Router from './src(myJopApi)/Router'
 
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Makeup);
+AppRegistry.registerComponent(appName, () => Router);
